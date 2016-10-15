799edfebf687788f014f33dece746e2e25a482c8
K1967 <valtteri.seuranen@gmail.com>
2016-10-15 23:43:52 +0300

    api construction
    käyttäjien hakeminen, tapahtumien hakeminen toimii

--- API/index.php
+++ API/index.php
@@ -1,4 +1,7 @@
 <?php
+require_once("../connect.php");
+
+
 $array = array(2,3,4,5);
 
 //200 = ok, 400 = no
@@ -7,10 +10,57 @@ $uri = explode("API/",$_SERVER["REQUEST_URI"]);
 echo json_encode($uri);
 */
 
-if(empty($_GET["index"])){
-    Response(400,"Response fail", null);
+if(empty($_GET["type"])){
+    Response(400,"Response fail, no data input", null);
 } else {
-    Response(200, "Response ok", $array[$_GET["index"]]);
+    
+    $parameter;
+    
+    switch($_GET["type"]){
+        case "event":
+            
+            $parameter = GetEventData($_GET["index"]);
+            
+            if($parameter == false){
+                Response(400, "Event does not exist", null);
+            } else {
+                Response(200, "Response ok", $parameter);    
+            }    
+            break;
+            
+        case "events":
+            
+            $parameter = GetTableData("event");
+            
+            if($parameter == null){
+                Response(400, "No events defined", null);
+            } else {
+                Response(200, "Response ok", $parameter);
+            }
+            break;
+            
+         case "user":
+            
+            $parameter = GetUserData($_GET["index"]);
+            
+            if($parameter == false){
+                Response(400, "User does not exist", null);
+            } else {
+                Response(200, "Response ok", $parameter);    
+            }    
+            break;
+            
+        case "users":
+            
+            $parameter = GetTableData("person");
+            
+            if($parameter == null){
+                Response(400, "No persons defined", null);
+            } else {
+                Response(200, "Response ok", $parameter);
+            }
+            break;
+    }
 }
 
 
@@ -20,8 +70,11 @@ function Response($status, $status_message, $data){
     
     $response["status"] = $status;
     $response["status_message"] = $status_message;
+    
     $response["data"] = $data; 
-        
+    
+    
+    
     echo json_encode($response);
 }
 
--- connect.php
+++ connect.php
@@ -8,6 +8,7 @@ function Connect(){
 
         $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec("SET CHARACTER SET utf8");
         return $conn;
         }
     catch(PDOExecption $ex){
@@ -18,16 +19,47 @@ function Connect(){
 function GetTableData($table){
      try {
         $conn = Connect();
-        $statement = $conn->prepare("SELECT * FROM ?;");
-        $statement->bindValue(1, $table, PDO::PARAM_STR);
+        
+        $table = htmlspecialchars($table);
+        $statement = $conn->prepare("SELECT * FROM $table;");
+        //$statement->bindValue(1, $table, PDO::PARAM_STR);
         $statement->execute();
-        $tulos = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $tulos = $statement->fetchAll(PDO::FETCH_ASSOC);;
         return $tulos;
 
     } catch(PDOException $e){
-        echo "error:" . $e->getCode();
-         print_r($e["file"]);
-
+        //echo "error:" . $e->getMessage();
     }
 }
+
+function GetEventData($event){
+     try {
+        $conn = Connect();
+        $event = htmlspecialchars($event);
+        $statement = $conn->prepare("SELECT * FROM event where id=?;");
+        $statement->bindValue(1, $event, PDO::PARAM_INT);
+        $statement->execute();
+        $tulos = $statement->fetch(PDO::FETCH_ASSOC);;
+        return $tulos;
+
+    } catch(PDOException $e){
+        //echo "error:" . $e->getMessage();
+    }
+}
+
+function GetUserData($user){
+     try {
+        $conn = Connect();
+        $user = htmlspecialchars($user);
+        $statement = $conn->prepare("SELECT * FROM event where id=?;");
+        $statement->bindValue(1, $user, PDO::PARAM_INT);
+        $statement->execute();
+        $tulos = $statement->fetch(PDO::FETCH_ASSOC);;
+        return $tulos;
+
+    } catch(PDOException $e){
+        //echo "error:" . $e->getMessage();
+    }
+}
+
 ?>
